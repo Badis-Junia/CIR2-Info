37bba0808133dfec6b66c54649fa0680babf5cae
Badis-Junia <badis.dahi@student.junia.com>
2025-09-11 07:27:56 +0200

    Auto-commit 2025-09-11 07:27:56

---
+++ EXOS/EXO1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int test;
+    std::cout << "Entrez la valeur que vous voulez : ";
+    std::cin >> test; 
+
+    std::cout << "Voilà la table de multiplication du nombre " << test << std::endl;
+    for(int i = 0;i<11;i++) {
+        std::cout << test << "x" << i << "=" << test * i << "" << " ";
+    }
+
+    return 0;
+}
