95d255a85c83fa40f0de1aff43bce66aa8c6535b
Badis-Junia <badis.dahi@student.junia.com>
2025-09-17 14:45:32 +0200

    Auto-commit 2025-09-17 14:45:32

---
+++ EXOS/EXO3/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+class Quadrupede {
+    private:
+        std::string nom;
+    public:
+        std::string get_name() {
+            return nom;
+        }
+        void set_name(std::string name) {
+            nom = name;
+        }
+};
+
+class Chien : public Quadrupede {
+    public:
+        Chien(std::string name) {
+            std::cout << "hey je suis le chien " << name << std::endl;
+            set_name(name);
+        }
+        void speak()  {
+            std::cout << "WAF" << std::endl;
+        }
+
+};
+
+
+class Chat : public Quadrupede {
+    public:
+        void speak()  {
+            std::cout << "MIAOU" << std::endl;
+        }
+
+
+};
+
+int main() {
+    Chat chat;
+    Chien chien("oiseau");
+    std::cout << chien.get_name() << std::endl;
+    chien.set_name("test");
+    std::cout <<chien.get_name() << std::endl;
+
+    chat.speak();
+    chien.speak();
+    return 0;
+}
