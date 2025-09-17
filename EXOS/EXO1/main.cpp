ae77734a57b8fb145c84c0805d6b56fe96a8addd
Badis-Junia <badis.dahi@student.junia.com>
2025-09-17 10:39:24 +0200

    Auto-commit 2025-09-17 10:39:24

--- EXOS/EXO1/main.cpp
+++ EXOS/EXO1/main.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 
 int main() {
-    int test;
-    std::cout << "Entrez la valeur que vous voulez : ";
-    std::cin >> test; 
-
-    std::cout << "Voilà la table de multiplication du nombre " << test << std::endl;
-    for(int i = 0;i<11;i++) {
-        std::cout << test << "x" << i << "=" << test * i << "" << " " << std::endl;
+    int nombre;
+    std::cout << "Choisissez le nombre que vous voulez mettre : ";
+    std::cin >> nombre;
+    for(int i  =0;i<10;i++) {
+        std::cout << i << "x" << nombre << "=" << nombre * i << " ";
     }
-
     return 0;
 }
---
+++ TD2/EXO5/readme.txt
@@ -0,0 +1,1 @@
+voir l'exercice 4, le 5 est dessus 
