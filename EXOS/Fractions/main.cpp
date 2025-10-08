e8ff782e650845a5f71570087a3e6bb591e9b007
Badis-Junia <badis.dahi@student.junia.com>
2025-10-08 15:28:38 +0200

    Auto-commit 2025-10-08 15:28:38

--- EXOS/Fractions/main.cpp
+++ EXOS/Fractions/main.cpp
@@ -4,42 +4,42 @@
 #include <algorithm>
 
 int main() {
-    Fraction f1(1, 3);
-    Fraction f2(1, 3);
+    // Fraction f1(1, 3);
+    // Fraction f2(1, 3);
 
 
 
-    std::cout << "f1: "; 
-    f1.print();
-    std::cout << std::endl;
+    // std::cout << "f1: "; 
+    // f1.print();
+    // std::cout << std::endl;
     
-    std::cout << "f2: "; 
-    f2.print();
-    std::cout << std::endl;
-///////////////////////////////////////////////////////////////////    
-    Fraction f3 = f1 + f2;
-    std::cout << "f3 (f1 + f2): "; 
-    f3.print(); // somme
-    std::cout << std::endl;
-///////////////////////////////////////////////////////////////////
-    Fraction f6 = f1 - f2;
-    std::cout << "f6 = (f1 - f2): "; 
-    f6.print(); // soustraction
-    std::cout << std::endl;
-///////////////////////////////////////////////////////////////////
-    Fraction f7 = f1 * f2;
-    std::cout << "f6 = (f1 * f2): "; 
-    f7.print(); // soustraction
-    std::cout << std::endl;
-///////////////////////////////////////////////////////////////////
-    Fraction f8 = f1 / f2;
-    std::cout << "f8 = (f1 / f2): "; 
-    f8.print(); // soustraction
+    // std::cout << "f2: "; 
+    // f2.print();
+    // std::cout << std::endl;
+// ///////////////////////////////////////////////////////////////////    
+    // Fraction f3 = f1 + f2;
+    // std::cout << "f3 (f1 + f2): "; 
+    // f3.print(); // somme
+    // std::cout << std::endl;
+// ///////////////////////////////////////////////////////////////////
+    // Fraction f6 = f1 - f2;
+    // std::cout << "f6 = (f1 - f2): "; 
+    // f6.print(); // soustraction
+    // std::cout << std::endl;
+// ///////////////////////////////////////////////////////////////////
+    // Fraction f7 = f1 * f2;
+    // std::cout << "f6 = (f1 * f2): "; 
+    // f7.print(); // soustraction
+    // std::cout << std::endl;
+// ///////////////////////////////////////////////////////////////////
+    // Fraction f8 = f1 / f2;
+    // std::cout << "f8 = (f1 / f2): "; 
+    // f8.print(); // soustraction
 
 
 
 
-    std::cout << std::endl;
+    // std::cout << std::endl;
 
 
 
@@ -58,7 +58,8 @@ int main() {
 
 
     std::sort(tab.begin(), tab.end());
-    
+    std::cout << std::endl;
+    std::cout << "Voilà le tableau trié \n" << std::endl;    
 
     for(long unsigned int i = 0;i<tab.size();i++) {
         tab[i].print();
