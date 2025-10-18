1c4ae298a8f3d9e7c572f65be9fcb47bd7a9a430
Badis-Junia <badis.dahi@student.junia.com>
2025-10-18 10:52:41 +0200

    Auto-commit 2025-10-18 10:52:41

--- EXOS/Fractions/main.cpp
+++ EXOS/Fractions/main.cpp
@@ -4,42 +4,6 @@
 #include <algorithm>
 
 int main() {
-    // Fraction f1(1, 3);
-    // Fraction f2(1, 3);
-
-
-
-    // std::cout << "f1: "; 
-    // f1.print();
-    // std::cout << std::endl;
-    
-    // std::cout << "f2: "; 
-    // f2.print();
-    // std::cout << std::endl;
-// ///////////////////////////////////////////////////////////////////    
-    // Fraction f3 = f1 + f2;
-    // std::cout << "f3 (f1 + f2): "; 
-    // f3.print(); // somme
-    // std::cout << std::endl;
-// ///////////////////////////////////////////////////////////////////
-    // Fraction f6 = f1 - f2;
-    // std::cout << "f6 = (f1 - f2): "; 
-    // f6.print(); // soustraction
-    // std::cout << std::endl;
-// ///////////////////////////////////////////////////////////////////
-    // Fraction f7 = f1 * f2;
-    // std::cout << "f6 = (f1 * f2): "; 
-    // f7.print(); // soustraction
-    // std::cout << std::endl;
-// ///////////////////////////////////////////////////////////////////
-    // Fraction f8 = f1 / f2;
-    // std::cout << "f8 = (f1 / f2): "; 
-    // f8.print(); // soustraction
-
-
-
-
-    // std::cout << std::endl;
 
     std::vector<Fraction> tab;    
     long unsigned int nombre;
