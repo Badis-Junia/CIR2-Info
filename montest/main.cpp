5a2b9d5f1786bc10fbeeb3f72456e7a710da3ff7
Badis-Junia <badis.dahi@student.junia.com>
2025-11-09 20:47:39 +0100

    Auto-commit 2025-11-09 20:47:39

--- montest/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-
-int main() {
-    std::vector<int> test = {1,2,4,1,2,9,4};
-    for(long unsigned int i = 0;i<test.size();i++) {
-        std::cout << test[i];
-    }
-}
